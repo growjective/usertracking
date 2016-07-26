9163ec636a8d1f5e108b9eeb7e1e3e27969e0926
HL <heyse.hli@gmail.com>
2016-07-27 00:42:22 +0700

    Change dev url to real url

--- src/lib/getRemoteUrl.js
+++ src/lib/getRemoteUrl.js
@@ -1,7 +1,8 @@
 /* eslint-disable import/no-unresolved */
 import { Meteor } from 'meteor/meteor';
 
-export const devUrl = 'http://localhost:4000/api/';
+// export const devUrl = 'http://localhost:4000/api/';
+export const devUrl = 'https://www.growjective.com/api/';
 export const prodUrl = 'https://www.growjective.com/api/';
 
 export default function getRemoteUrl(route = '') {
